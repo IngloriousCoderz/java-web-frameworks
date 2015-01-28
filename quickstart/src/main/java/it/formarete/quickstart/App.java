1eff1fbd49457cab01ba90ca8aa8c4b7bde27eac
IceOnFire <antony.mistretta@gmail.com>
2015-01-28 21:35:19 +0100

    Updated App class as POJO and created basic unit test.

--- quickstart/src/main/java/it/formarete/quickstart/App.java
+++ quickstart/src/main/java/it/formarete/quickstart/App.java
@@ -1,13 +1,17 @@
 package it.formarete.quickstart;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.Serializable;
+
+public class App implements Serializable {
+	private static final long serialVersionUID = -1998213895574757883L;
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
