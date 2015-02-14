9013d5eef5484d821ef9d0ecf42b1a2565313116
IceOnFire <antonymistretta@gmail.com>
2015-02-14 17:17:26 +0100

    Added project hiberstruts.

---
+++ hiberstruts/src/main/java/it/formarete/service/RegisterDao.java
@@ -0,0 +1,21 @@
+package it.formarete.service;
+
+import it.formarete.model.User;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class RegisterDao {
+	public static int saveUser(User u) {
+		Session session = new Configuration().configure("hibernate.cfg.xml")
+				.buildSessionFactory().openSession();
+
+		Transaction t = session.beginTransaction();
+		int i = (Integer) session.save(u);
+		t.commit();
+		session.close();
+
+		return i;
+	}
+}
