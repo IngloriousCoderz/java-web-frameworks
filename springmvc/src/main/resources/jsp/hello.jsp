0ae3f1292eaa3be5e45a353defcf0ab88e0f991c
IceOnFire <antony.mistretta@gmail.com>
2016-11-03 19:50:17 +0100

    Add minimal SpringMVC example

---
+++ springmvc/src/main/resources/jsp/hello.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hello Spring MVC</title>
+</head>
+<body>
+	<h2>${message}</h2>
+</body>
+</html>
--- struts/src/test/java/it/formarete/test/LoginTestCase.java
+++ struts/src/test/java/it/formarete/test/LoginTestCase.java
@@ -1,44 +1,58 @@
 package it.formarete.test;
 
-import it.formarete.action.Auth;
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
 
 import com.opensymphony.xwork2.Action;
 
-public class LoginTestCase extends TestCase {
+import it.formarete.action.Auth;
+
+public class LoginTestCase {
 	private Auth auth;
 
+	@Before
 	public void setUp() {
 		auth = new Auth();
 	}
 
+	@Test
 	public void testAlreadyLoggedIn() throws Exception {
 		auth.setLogin("giancarlo");
 		assertEquals(Action.SUCCESS, auth.execute());
 	}
 
+	@Test
 	public void testNoLoginAttempts() throws Exception {
 		assertEquals(Action.LOGIN, auth.execute());
 	}
 
+	@Test
 	public void testWrongUsername() throws Exception {
 		auth.setUsername("pippo");
 		auth.setPassword("baudo");
 		assertEquals(Action.INPUT, auth.execute());
 	}
 
+	@Test
 	public void testWrongPassword() throws Exception {
 		auth.setUsername("giancarlo");
 		auth.setPassword("nonparlo");
 		assertEquals(Action.INPUT, auth.execute());
 	}
 
+	@Test
 	public void testSuccessfulLogin() throws Exception {
 		auth.setUsername("giancarlo");
 		auth.setPassword("magalli");
 		assertEquals(Action.SUCCESS, auth.execute());
 	}
 
+	@Test
 	public void testLogout() throws Exception {
 		auth.setLogin("giancarlo");
 		assertEquals("giancarlo", auth.getLogin());
